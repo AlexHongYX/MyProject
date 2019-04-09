ace0004804e0b63bca25a674757e620770685d2c
xiaomessi <921721870@qq.com>
2019-04-09 23:15:52 +0800

    End ClearMine

--- MineClear/MineClear/game.c
+++ MineClear/MineClear/game.c
@@ -35,23 +35,52 @@ void printArray(char print_map[ROW][COL], int rows, int cols){
 	printf("\n\n");
 }
 
+//随机生成10个雷
 void random_mine(char mine_map[ROW][COL], int rows, int cols){
-	int count = 0;
+	int count = COUNT;
 	int row = 0;
 	int col = 0;
-	while (1){
+	while (count>0){
 		row = rand() % rows;
 		col = rand() % cols;
-		if (mine_map[row][col] == '0'){
-			mine_map[row][col] = '1';
-			++count;
-			if (count == 10){
-				break;
-			}
+		if (mine_map[row][col] == '1'){
+			continue;
 		}
+		mine_map[row][col] = '1';
+		--count;
 	}
-	
+}
 
+//翻开后更新一个数字代表周围8个格子有几个雷
+void update_show(char show_map[ROW][COL], char mine_map[ROW][COL], int rows, int cols,int row,int col){
+
+	int count = 0;
+	//判断row,col周围的雷
+	if (row - 1 >= 0 && col - 1 >= 0 && mine_map[row - 1][col - 1] == '1'){
+		++count;
+	}
+	if (row - 1 >= 0 && mine_map[row - 1][col] == '1'){
+		++count;
+	}
+	if (row - 1 >= 0 && col + 1 <cols && mine_map[row - 1][col + 1] == '1'){
+		++count;
+	}
+	if (col - 1 >=0 && mine_map[row][col - 1] == '1'){
+		++count;
+	}
+	if (col + 1 <cols && mine_map[row][col + 1] == '1'){
+		++count;
+	}
+	if (row + 1 <rows && col - 1 >=0 && mine_map[row + 1][col - 1] == '1'){
+		++count;
+	}
+	if (row + 1 <rows && mine_map[row + 1][col] == '1'){
+		++count;
+	}
+	if (row + 1 <rows && col + 1 <cols && mine_map[row + 1][col + 1] == '1'){
+		++count;
+	}
+	show_map[row][col] = count + '0';
 }
 
 void game(){
@@ -74,10 +103,41 @@ void game(){
 	printArray(show_map,ROW,COL);
 	printArray(mine_map, ROW, COL);
 
-	//让用户输入一组坐标，需要检查坐标的合法性
-	printf("请输入坐标：");
-	scanf("%d%d", &row, &col);
-
-
+	
+	int count = 0;
+	while (1){
+		//让用户输入一组坐标，需要检查坐标的合法性
+		printf("请输入坐标(row col)：");
+		scanf("%d%d", &row, &col);
+		if (row<0 || row >= ROW || col<0 || col >= COL){
+			printf("请输入正确的坐标\n");
+			continue;
+		}
+		if (show_map[row][col] != '*'){
+			printf("该位置已被打开，请重新输入\n");
+			continue;
+		}
+		if (mine_map[row][col] == '1'){
+			printf("GameOver!\n");
+			printArray(mine_map, ROW, COL);
+			break;
+		}
+		if (row >= 0 && row < ROW&&col >= 0 && col < COL&&show_map[row][col] == '*'){
+			update_show(show_map,mine_map, ROW, COL,row,col);
+			//用于测试
+			//show_map[row][col] = '#';
+		}
+		count++;
+		if (count == ROW*COL - COUNT){
+			printArray(mine_map, ROW, COL);
+			printf("You Win\n");
+			break;
+		}
+		//打印数组
+		printArray(show_map, ROW, COL);
+		printArray(mine_map, ROW, COL);
+	}
+	//如果跳出来则游戏结束
+	
 
 }
--- MineClear/MineClear/game.h
+++ MineClear/MineClear/game.h
@@ -2,16 +2,24 @@
 
 #define ROW 9
 #define COL 9
+#define COUNT 10
 
 #include<stdio.h>
 
+//菜单
 void menu();
 
+//游戏主函数
 void game();
 
+//初始化
 void init(char show_map[ROW][COL], char mine_map[ROW][COL], int rows, int cols);
 
+//打印输出
 void printArray(char print_map[ROW][COL], int rows, int cols);
 
+//随机生成雷
 void random_mine(char mine_map[ROW][COL], int rows, int cols);
 
+//更新显示
+void update_show(char show_map[ROW][COL], char mine_map[ROW][COL], int rows, int cols, int row, int col);
